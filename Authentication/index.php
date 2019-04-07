f74698be176e54138e877db745e84f7494aaa3b2
Egahi <okworisamuel6@gmail.com>
2019-04-07 15:24:38 +0100

    implemented UI of login page

--- Authentication/index.php
+++ Authentication/index.php
@@ -4,23 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/home-page.css">
-    <title>Document</title>
+    <title>Home Page</title>
 </head>
 <body>
-    <div class="box">
-        <img class="home-logo" src="../img/DB.png" alt="FB Image">
-        <h3>Login to DB</h3>
-        <p>Click on the following to proceed</p>
-        <div class="social-links">
-            <a href="#">
-                <img class="social-icon" src="../img/fb.png" alt="">Facebook
-            </a>
-            <a href="#">
-                <img class="social-icon" src="../img/google.png" alt="">Google
-            </a>
-        </div>
-        <p class="conditions">By logging in you agree to <a href="#">Terms &amp; Conditions</a><br>and <a href="#">Privacy Policy</a> of DB.</p>
-    </div>
+   <!-- Landing page code goes here-->
 </body>
 </html>
