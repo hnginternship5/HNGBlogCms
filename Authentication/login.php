5877f91beca0e3caffec8238ef4b3659eaf3da73
Okafor Irene <okaforirenen@gmail.com>
2019-04-07 14:18:07 +0100

    added favicon to title

--- Authentication/login.php
+++ Authentication/login.php
@@ -23,7 +23,8 @@ if (isset($_SESSION['access_token'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="../css/authentication.css">
-        <title>Document</title>
+        <title>DragonBones</title>
+        <link rel="shortcut icon" href="./favicon.ico" />
     </head>
 
     <body>
