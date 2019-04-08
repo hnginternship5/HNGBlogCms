809052a18ae7bf112eb774e03e38370923917b0d
DmatrixNg <49094365+DmatrixNg@users.noreply.github.com>
2019-04-08 13:03:19 +0100

    new branch

--- dragonglass/app.js
+++
@@ -1,7 +0,0 @@
-const toolbar = document.querySelector('.toolbar')
-const editor = document.querySelector('.editor')
-editor.addEventListener('click', (e)=> {
-    setTimeout(()=> {
-        toolbar.style.opacity = 1
-    }, 500)
-})
