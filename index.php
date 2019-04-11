2b0de9cf9ae9ba8447e59de43cb1682c22e78632
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-12 00:04:09 +0100

    Add font warning

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ include 'header.php';
 
 <!-- insert new FE code below this line -->
 
+<!-- only use fonts approved by the designers -->
+<!-- otherwise, don't specify a font -->
+
 <!-- auth modal -->
 <?php include 'auth.php'; ?>
 
